453041b6c1da91a0cebf6f824670cace9e314f29
Flawid DSouza <me@flawiddsouza.com>
2025-01-14 21:35:29 +0530

    plugins: scribe renderer > move away from prosemirror example setup and do your own base setup

--- plugins/scribe-renderer/src/editor.ts
+++ plugins/scribe-renderer/src/editor.ts
@@ -1,13 +1,65 @@
-import { EditorState } from 'prosemirror-state';
+import { Command, EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Schema, Slice, Fragment, Node } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
 import { addListNodes } from 'prosemirror-schema-list';
-import { exampleSetup } from 'prosemirror-example-setup';
+import { history, undo, redo } from 'prosemirror-history';
+import { keymap } from 'prosemirror-keymap';
+import { baseKeymap, toggleMark, chainCommands, exitCode } from 'prosemirror-commands';
+import { undoInputRule } from 'prosemirror-inputrules';
+import { dropCursor } from 'prosemirror-dropcursor';
+import { gapCursor } from 'prosemirror-gapcursor';
 import 'prosemirror-view/style/prosemirror.css';
-import 'prosemirror-example-setup/style/style.css';
 import 'prosemirror-gapcursor/style/gapcursor.css';
 import './main.css';
+
+// Most of this is from: https://github.com/ProseMirror/prosemirror-example-setup/blob/master/src/keymap.ts
+function createCustomKeyMap(schema: Schema) {
+  const mac = typeof navigator != 'undefined' ? /Mac|iP(hone|[oa]d)/.test(navigator.platform) : false;
+
+  const customKeyMap: { [key: string]: Command } = {};
+
+  // undo / redo
+
+  customKeyMap['Mod-z'] = undo;
+  customKeyMap['Shift-Mod-z'] = redo;
+
+  customKeyMap['Backspace'] = undoInputRule;
+
+  if (!mac) {
+    customKeyMap['Mod-y'] = redo;
+  }
+
+  // bold
+
+  customKeyMap['Mod-b'] = toggleMark(schema.marks.strong);
+  customKeyMap['Mod-B'] = toggleMark(schema.marks.strong);
+
+  // italic
+
+  customKeyMap['Mod-i'] = toggleMark(schema.marks.em);
+  customKeyMap['Mod-I'] = toggleMark(schema.marks.em);
+
+  // Make shift + enter work
+
+  const br = schema.nodes.hard_break;
+
+  const lineBreakCmd = chainCommands(exitCode, (state, dispatch) => {
+    if (dispatch) {
+      dispatch(state.tr.replaceSelectionWith(br.create()).scrollIntoView());
+    }
+    return true;
+  });
+
+  customKeyMap['Mod-Enter'] = lineBreakCmd;
+  customKeyMap['Shift-Enter'] = lineBreakCmd;
+
+  if (mac) {
+    customKeyMap['Ctrl-Enter'] = lineBreakCmd;
+  }
+
+  return customKeyMap;
+}
 
 export function createEditor(mountPoint: HTMLElement, fileContent: string, updateCallback: (updatedValue: string) => void) {
   // Mix the nodes from prosemirror-schema-list into the basic schema to
@@ -29,7 +81,13 @@ export function createEditor(mountPoint: HTMLElement, fileContent: string, updat
   const view = new EditorView(mountPoint, {
     state: EditorState.create({
       doc: mySchema.nodeFromJSON(content),
-      plugins: exampleSetup({ schema: mySchema, menuBar: false }),
+      plugins: [
+        keymap(baseKeymap),
+        keymap(createCustomKeyMap(mySchema)),
+        dropCursor(),
+        gapCursor(),
+        history(),
+      ],
     }),
     // From: https://github.com/bluesky-social/social-app/pull/6658/files
     clipboardTextParser(text, context) {
